a860d9e8d8e6ba6e0f66efe027caf30013c96637
mj <ui2939@gmail.com>
2024-05-23 17:56:25 +0900

    edit

--- lessonNODE/app.js
+++ lessonNODE/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('session');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
 
-
 app.set('port', process.env.PORT || 3000);
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.use((req, res, next)=>{
+    console.log("모든 요청");
+    next();  //다음 미들웨어로 넘어가라
+});
 
 app.get('/', (req, res)=>{
     res.send("express main page");
@@ -11,3 +28,5 @@ app.get('/', (req, res)=>{
 app.listen(3000, ()=>{
     console.log("Server is running");
 });
+
+// app.get('/img', ()=>{});
