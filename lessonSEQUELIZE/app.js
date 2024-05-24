cabf40de5f84c649bd8e13edf8d07b573996e315
mj <ui2939@gmail.com>
2024-05-24 18:17:45 +0900

    create lesson sequelize folder

---
+++ lessonSEQUELIZE/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const {sequelize} = require('./models');
+const app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+nunjucks.configure('views',{express:app, watch:false}); //models 완료후 true로
+
+sequelize.sync({force: false}).then(()=>{console.log('DB 연결됨')})
+.catch((err)=>{console.error(err)});
+
+app.listen(app.get("port"), ()=>{console.log("DB 접속");})
