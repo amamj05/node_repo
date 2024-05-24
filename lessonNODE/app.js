1f8ca040be93313f0537e22aa074ded46748927b
mj <ui2939@gmail.com>
2024-05-24 10:45:10 +0900

    multer install

--- lessonNODE/app.js
+++ lessonNODE/app.js
@@ -4,6 +4,12 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('session');
 const dotenv = require('dotenv');
+const { sign } = require('crypto');
+//
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.raw());
+app.use(express.text());
 
 dotenv.config();
 const app = express();
@@ -14,6 +20,7 @@ app.set('port', process.env.PORT || 3000);
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 
 app.use((req, res, next)=>{
@@ -21,12 +28,60 @@ app.use((req, res, next)=>{
     next();  //다음 미들웨어로 넘어가라
 });
 
+app.use((err, req, res, next)=>{
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.use(session({resave:false, saveUninitialized:false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {httpOnly:true, secure:false},
+    name: 'session-cookie'
+}));
+
 app.get('/', (req, res)=>{
     res.send("express main page");
 });
 
-app.listen(3000, ()=>{
+app.get("/html",(req, res)=>{
+    res.sendFile(Path.join(__dirname, 'html.html'));
+});
+
+
+
+app.get("cookie_set", (req, res)=>{
+    res.cookie("username","test01",{
+        expires: new Date(Date.now()+30000),
+        httpOnly: true, secure: true, signed:true
+    })
+});
+
+app.get("/cookie_reset",(req, res)=>{
+    res.clearCookie("username","test01",{
+        httpOnly:true, secure, signed:true
+    })
+    res.send("!!! cookie reset !!!")
+});  
+
+// app.listen(3000, ()=>{
+//     console.log("Server is running");
+// });
+
+app.listen(app.get('port'), ()=>{
     console.log("Server is running");
 });
 
-// app.get('/img', ()=>{});
+
+// multipart -> multer 이미지 업로드
+const MYmulter = require('multer');
+const mul_upload = multer({
+    storage: multer.diskStorage({ destination(req, file, done){
+        done(null, 'uploads/');
+    },
+    filename(req, file, done){
+        const ext = path.extname(file.originalname);
+        done(null, path.basename(file.originalname, ext) + Date.now()+ext);  //ext가 확장자? 
+    }
+}), 
+    limits: {fileSize: 5 *1024*1024}
+});  //fileSize : 5MB
