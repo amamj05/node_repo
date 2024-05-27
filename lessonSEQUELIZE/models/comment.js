2484955744afc4a400fd9fb21970659f82d82e46
mj <ui2939@gmail.com>
2024-05-27 10:38:21 +0900

    lessonSEQUELIZE models add column type

--- lessonSEQUELIZE/models/comment.js
+++ lessonSEQUELIZE/models/comment.js
@@ -1,7 +1,10 @@
 const Sequelize = require('sequelize');
 
 class comment extends Sequelize.Model{
-    static initiate(sequelize){}
+    static initiate(sequelize){comment.init({
+        comment:{},
+        created_at:{}  //언제 만들어졌는지
+    },{});}
     static associate(db){}
 }
 module.exports = comment;
--- lessonSEQUELIZE/models/user.js
+++ lessonSEQUELIZE/models/user.js
@@ -1,7 +1,23 @@
 const Sequelize = require('sequelize');
 
 class User extends Sequelize.Model{
-    static initiate(sequelize){}
+    static initiate(sequelize){User.init({
+        name:{type: Sequelize.STRING(20),
+            allowNull: false, //not Null
+            unique: true
+        },
+        age:{type: Sequelize.INTEGER.UNSIGNED,
+            // INTEGER 정수형 UNSIGNED 양수
+            allowNull: false
+        },
+        comment:{type: Sequelize.TEXT,  //외래키 해야함
+            allowNull: false
+        },  
+        created_at:{type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue:Sequelize.Now
+        }  
+    },{});}
     static associate(db){}
 }
 module.exports = User;
