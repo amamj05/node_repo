706f8ade05788f7595196aaee7c73bb3ce16cead
mj <ui2939@gmail.com>
2024-05-27 15:28:34 +0900

    lessonSEQUELIZE edit sequelize.js

--- lessonSEQUELIZE/public/sequelize.js
+++ lessonSEQUELIZE/public/sequelize.js
@@ -1,3 +1,25 @@
 document.querySelectorAll('#usesr-list tr').forEach((el)=>{el.addEventListener('click', function(){
-    const id_func = el.
+    const id_func = el.querySelector('td');
+    getComment(id);
 });});
+
+
+async function getUser(){
+    try{
+        const res = await axios.get('/users');
+        const users = res.date;
+        const tbody = document.querySelector('#user-list tbody');
+        tbody.innerHTML ='';
+        users.map(function(user){
+            const row = document.createElement('tr');
+            row.addEventListener('click', ()=>{getComment(user.id);});
+            let td = document.createElement('td');
+            td.textContent = user.id;
+            row.appendChild(td);
+            //625p
+        });
+    }catch(err){console.error(err);}
+}
+
+
+function getComment(id){}
